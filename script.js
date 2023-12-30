37a8365f0dfb1d4b160810e52268d2051c94f815
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-30 13:00:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+            // Get the body element and set its innerHTML to "DOM load success"
+            document.body.innerHTML = "DOM load success";
+        });
